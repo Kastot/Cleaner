1d0154eca56e61b15eec5ff6f7ee92f70a102dc8
Kastot <153393126+Kastot@users.noreply.github.com>
2025-03-04 04:59:44 -0500

    CleanerWithGui.py
    I just added a nice looking GUI.

--- test.py
+++ test.py
@@ -1,103 +1,139 @@
-import shutil, os, webbrowser, subprocess, winreg, tkinter as tk, time, ctypes, urllib.request
+import shutil, os, webbrowser, subprocess, time, ctypes, urllib.request
+from plyer import notification
+import tkinter as tk
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock  # To create the countdown with Kivy
 
+# Define the functions based on your script
+import shutil, os, webbrowser, subprocess, winreg, tkinter as tk, time, ctypes, urllib.request
 
 ###################################################################################
 def Codescribe():
     webbrowser.open("http://codescribe.tilda.ws")
 
-def Rick():
-    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
 Codescribe()
 
-time.sleep(1)
-
-Rick()
 
 ###################################################################################
 
-DelTemp = 'c:\windows\Temp'
-for filename in os.listdir(DelTemp):
-    file_path = os.path.join(DelTemp, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-###################################################################################
-
-        DelBin = 'C:\$Recycle.Bin'
-for filename in os.listdir(DelBin):
-    file_path = os.path.join(DelBin, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-###################################################################################
-
-        DelLogs = 'C:\\Windows\\Logs\\'
-for filename in os.listdir(DelLogs):
-    file_path = os.path.join(DelLogs, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-###################################################################################
-
-subprocess.run(['cleanmgr', '\sagerun:1'], shell=True)
-
-subprocess.run(['reg', 'add', 'HKCU\\Software\\Microsoft\\Windows\\DWM', '/v', 'EnableTransparency', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
-
-subprocess.run(['reg', 'add', 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Search', '/v', 'CortanaConsent', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
-
-subprocess.run(['reg', 'add', 'HKCU\\Control Panel\\Desktop', '/v', 'WindowAnimation', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
-
-
-
-
-
-
-
-import ctypes
-import urllib.request
-import os
-
-def change_wallpaper():
-    # URL of the image you want to use as wallpaper
-    image_url = "https://www.boredpanda.com/blog/wp-content/uploads/2024/01/funny-weird-some-images-10-659bfc19cb0bf-png__700.jpg"
+def run_cleanup_script():
     
-    # Path where the image will be saved temporarily
-    image_path = os.path.join(os.getenv('TEMP'), "temp_wallpaper.jpg")
+    DelTemp = 'c:\windows\Temp'
+    for filename in os.listdir(DelTemp):
+        file_path = os.path.join(DelTemp, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    ###################################################################################
+
+    DelBin = 'C:\$Recycle.Bin' 
+    for filename in os.listdir(DelBin):
+        file_path = os.path.join(DelBin, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    ###################################################################################
+
+    DelLogs = 'C:\Windows\Logs' 
+    for filename in os.listdir(DelLogs):
+        file_path = os.path.join(DelLogs, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    ###################################################################################
+
+    subprocess.run(['cleanmgr', '/sagerun:1'], shell=True)
+
+    subprocess.run(['reg', 'add', 'HKCU\\Software\\Microsoft\\Windows\\DWM', '/v', 'EnableTransparency', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
+
+    subprocess.run(['reg', 'add', 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Search', '/v', 'CortanaConsent', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
+
+    subprocess.run(['reg', 'add', 'HKCU\\Control Panel\\Desktop', '/v', 'WindowAnimation', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
+
+
+
+class MyApp(App):
+    def build(self):
+        
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10, )
+        
+        
+        
+        self.label = Label(text="Click the button to clean your system", font_size=32, color=(1, 1, 1, 1))
+        layout.add_widget(self.label)
+
+       
+        button = Button(text="Run Cleanup Script", size_hint=(None, None), size=(200, 100))
+
+       
+        button.bind(on_press=self.on_click)
+
+        layout.add_widget(button)
+
+        return layout
+
+    def on_click(self, instance):
+        
+        self.label.text = "Running the script..."
+
+        
+        run_cleanup_script()
+
+       
+        self.start_countdown()
+
+    def start_countdown(self):
+       
+        self.label.text = "Self destructing in..."
+     
+        Clock.schedule_once(self.start_countdown_2, 3)
+
+    def start_countdown_2(self, dt):
+       
+        self.label.text = "3"
+        
+        Clock.schedule_once(self.update_countdown_2, 1)
+
+    def update_countdown_2(self, dt):
+        
+        self.label.text = "2"
+       
+        Clock.schedule_once(self.update_countdown_3, 1)
+
+    def update_countdown_3(self, dt):
     
-    # Download the image to a temporary file
-    try:
-        urllib.request.urlretrieve(image_url, image_path)
-        print("Image downloaded successfully.")
-    except Exception as e:
-        print(f"Error downloading image: {e}")
-        return
+        self.label.text = "1"
+        
+        Clock.schedule_once(self.final_message, 2)
 
-    # Set the downloaded image as the wallpaper
-    try:
-        ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
-        print("Wallpaper changed successfully.")
-    except Exception as e:
-        print(f"Error setting wallpaper: {e}")
-    
-    # Optionally, delete the temporary image file after setting the wallpaper
-    os.remove(image_path)
+    def final_message(self, dt):
+        
+        self.label.text = "Jk"
+        Clock.schedule_once(self.Greatday, 2)
 
-# Run the function to change the wallpaper
-change_wallpaper()
+    def Greatday(self, dt):
+        
+        self.label.text = "Your computer is clean now, have a great day. --Emi"
 
+
+
+if __name__ == "__main__":
+    MyApp().run()
