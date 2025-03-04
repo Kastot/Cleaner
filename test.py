b573c7e1ac4423ba56fcf161fb28315b92c0177e
Kastot <153393126+Kastot@users.noreply.github.com>
2025-03-03 23:14:05 -0500

    test.py
    Rick astley

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-import shutil, os, webbrowser
+import shutil, os, webbrowser, subprocess, winreg, tkinter as tk, time
 
-def open_browser():
-    webbrowser.open("http://codescribe.tilda.ws")  
-open_browser()
+
+
+def Codescribe():
+    webbrowser.open("http://codescribe.tilda.ws")
+
+def Rick():
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+Codescribe()
+
+time.sleep(2)
+
+Rick()
 folder = 'c:\windows\Temp'
 
 for filename in os.listdir(folder):
@@ -16,9 +26,20 @@ for filename in os.listdir(folder):
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-        folder2 = 'C:\$Recycle.Bin'
-for filename in os.listdir(folder2):
-    file_path = os.path.join(folder2, filename)
+        DelBin = 'C:\$Recycle.Bin'
+for filename in os.listdir(DelBin):
+    file_path = os.path.join(DelBin, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+        DelLogs = 'C:\\Windows\\Logs\\'
+for filename in os.listdir(DelLogs):
+    file_path = os.path.join(DelLogs, filename)
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
@@ -28,13 +49,4 @@ for filename in os.listdir(folder2):
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-        folder3 = 'C:\\windows\\prefetch'
-for filename in os.listdir(folder3):
-    file_path = os.path.join(folder3, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+    
